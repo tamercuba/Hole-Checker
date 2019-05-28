4aac2c12023bd967a9a967d09145ce96c0f7fc27
tamercuba <tamercuba@gmail.com>
2019-05-28 11:43:27 -0300

    testes de palavras erradas adicionado

--- tests.py
+++ tests.py
@@ -4,21 +4,32 @@ import unittest
 import app
 
 class HoleCheckerTestCase(unittest.TestCase):
-    def setUp(self):
-        self.words = {
+    '''
+    Classe de teste para a função que checa buracos
+    '''
+    def setUp(self):  # Define os valores que serão testados
+        self.right_words = {
             'Laranja': 3,
             'Brasília': 4,
             'Balestra': 5,
-            'Absurdo': 3,
+            'Absurdo': 4,
             'Alarme': 3,
             'Calamidade': 6,
             'Milho': 1,
             'City': 0,
             '': 0,
         }
-    def test_holes(self):
-        for key, value in self.words.items():
-            self.assertEqual(app.hole_checker(key), value)
+        self.wrong_words = {
+            'T4mer': 'A palavra inserida deve conter apenas letras',
+            '123': 'A palavra inserida deve conter apenas letras'
+        }
 
+    def test_holes_correct(self):  # Testa os valores previamente definidos
+        for key, value in self.right_words.items():
+            self.assertEqual(app.checa_buracos(key),value)
+
+    def test_wrong_holes(self):
+        for key in self.wrong_words.keys():
+            self.assertRaises(KeyError, lambda: app.checa_buracos(key))
 if __name__=='__main__':
     unittest.main()
