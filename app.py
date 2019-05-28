b5e6524b46a77b8ced7cd4175db0f0bfe7f4e97f
tamercuba <tamercuba@gmail.com>
2019-05-28 11:11:29 -0300

    funcao checa buracos finalizada + remove acentos

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python
+
+from unicodedata import normalize
+import re
+
 template = {
     'A': 1, 'B': 2, 'C': 0, 'D': 1, 'E': 0, 'F': 0, 'G': 0, 'H': 0, 'I':0,
     'J': 0, 'K': 0, 'L': 0, 'M': 0, 'N': 0, 'O': 1, 'P': 1, 'Q': 1, 'R': 1,
@@ -6,18 +10,33 @@ template = {
 
     'a': 1, 'b': 1, 'c': 0, 'd': 1, 'e': 1, 'f': 0, 'g': 0, 'h': 0, 'i': 0,
     'j': 0, 'k': 0, 'l': 0, 'm': 0, 'n': 0, 'o': 1, 'p': 1, 'q': 1, 'r': 0,
-    's': 0, 't': 0, 'u': 0, 'v': 0, 'x': 0, 'z': 0, 'y': 0, 'w': 0 
+    's': 0, 't': 0, 'u': 0, 'v': 0, 'x': 0, 'z': 0, 'y': 0, 'w': 0
 }
+def remove_acentos(word):
+    '''
+    Função que remove acentos e cedilhas de uma string
+    '''
+    return normalize('NFKD', word).encode('ASCII', 'ignore').decode('ASCII')
 
-def hole_checker(word):
+def checa_buracos(inputword):
     '''
     Função que checa quantos buracos tem uma string
     '''
+    word = str(inputword)
+    if len(word) == 0:
+        return 0
+    else:
+        if not word.isalpha():
+            raise ValueError('Palavra com sintaxe incorreta')
+        palavra = remove_acentos(word)
+        buracos = 0
+        for letra in palavra:
+            buracos += template[letra]
 
-    pass
+    return buracos
 
 
 if __name__ == "__main__":
-    word = str(input("Digite uma palavra (apenas letras): "))
-    holes = hole_checker(word)
-    print(word,"tem ", holes, "buracos.")
+    palavra = str(input("Digite uma palavra (apenas letras): "))
+    buracos = checa_buracos(palavra)
+    print(palavra,"tem ", buracos, "buracos.")
